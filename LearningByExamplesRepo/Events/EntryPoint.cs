851d098ae1b0a0ea3ff27d834ad00157459e29eb
Dimitar Nozharov <dimitar.nozharov@gmail.com>
2022-08-26 08:33:00 +0300

    Adding a simple Publish-Subscriber event model

---
+++ LearningByExamplesRepo/Events/EntryPoint.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    /// <summary>
+    /// Events: when something is happend, the event is fired and it executes some code. 
+    /// The object that hold the event is publisher.
+    /// The methods that are added to that events are subscribers (respond to the event)
+    /// The subscriber (EntryPoint) doesn't know anything about the publisher (Shooter) - no code mix between two, only the possibility to subscribe for the event
+    /// </summary>
+    class EntryPoint
+    {
+
+        private static int score = 0;
+
+        static void Main(string[] args)
+        {
+            var shooter = new Shooter();
+            //Subscribe to the KillingCompleted event and call the KilledEnemy and AddScore of the subscriber.
+            shooter.ShotsFired += KilledEnemy;
+            shooter.ShotsFired += AddScore;
+
+            shooter.OnShoot();
+
+        }
+
+        public static void KilledEnemy(object sender, ShotsFiredEventArgs e)
+        {
+            var tempSender = sender as Shooter;
+
+            Console.WriteLine($"I am {tempSender.Name} and killed an enemy! My Score is: { score} ");
+            Console.WriteLine($"Date: {e.TimeOfKill.ToString("MM.dd.yyyy hh:mm:ss")}");
+        }
+
+        //When we kill enemy, add the score
+        public static void AddScore(object sender, EventArgs e)
+        {
+            score += 1;
+        }
+
+    }
+}
---
+++ LearningByExamplesRepo/Events/Shooter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    /// <summary>
+    /// Publisher which will trigger the event
+    /// </summary>
+    public class Shooter
+    {
+        private Random rng = new Random();
+
+        //public delegate void ShootingHandler(object sender, ShotsFiredEventArgs e);
+        //public event ShootingHandler ShotsFired;
+
+        //use this instead of the previous 2 lines
+        public event EventHandler<ShotsFiredEventArgs> ShotsFired;
+
+        public string Name { get; set; } = "Billy";
+
+        public void OnShoot()
+        {
+            while (true)
+            {
+                if(rng.Next(0,100) %2 == 0)
+                {
+                     //Shooter class is raising the event -> this
+                     ShotsFired?.Invoke(this, new ShotsFiredEventArgs());
+                }
+                else
+                {
+                    Console.WriteLine("I missed");
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
